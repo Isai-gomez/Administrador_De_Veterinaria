3352c719eb839fd9161aa1cc3b6a045ea6c074d1
Chai <isaigomezhernandez.com>
2019-10-11 13:18:15 -0500

    Guardando en el Local Storage

--- src/component/App.js
+++ src/component/App.js
@@ -9,6 +9,18 @@ class App extends Component {
       citas: {}
     };
   }
+  componentDidMount() {
+    const citasLS = localStorage.getItem("citas");
+    if (citasLS) {
+      this.setState({
+        citas: JSON.parse(citasLS)
+      });
+    }
+  }
+  componentDidUpdate() {
+    localStorage.setItem("citas", JSON.stringify(this.state.citas));
+  }
+
   crearCita = infoCita => {
     //Tomar una copia del estate
     const citas = { ...this.state.citas };
